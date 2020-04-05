be92a8228baa5cc8886169130f765e2c4eb1ecd6
Dmitrii Gushcha <gushcha.dmitrii@gmail.com>
2020-04-05 20:40:44 +0300

    stage 2 done

--- File Type Analyzer/task/src/analyzer/Analyzer.java
+++ File Type Analyzer/task/src/analyzer/Analyzer.java
@@ -1,11 +1,13 @@
 package analyzer;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 public class Analyzer {
     private Path file;
@@ -19,16 +21,73 @@ public class Analyzer {
         patternTypes.put(pattern, fileType);
     }
 
-    public String analyze() {
-        try (var reader = Files.newBufferedReader(file)) {
-            for (String pattern : patternTypes.keySet()) {
-                if (reader.lines().anyMatch(line -> Pattern.compile(pattern).matcher(line).find())) {
-                    return patternTypes.get(pattern);
-                }
+    public String analyze(String alg) {
+        try (var bis = new BufferedInputStream(Files.newInputStream(file))) {
+            var bytes = bis.readAllBytes();
+            switch (alg) {
+                case "--naive":
+                    return naive(bytes);
+                case "--KMP":
+                    return kmp(bytes);
+                default:
+                    return "Unknown algorithm";
             }
         } catch (IOException e) {
             e.printStackTrace();
+            return "Error occurred while working with file";
+        }
+    }
+
+    private String naive(byte[] bytes) {
+        for (String pattern : patternTypes.keySet()) {
+            var patternBytes = pattern.getBytes();
+
+            for (int i = pattern.length(); i <= bytes.length; i++) {
+                if (Arrays.equals(patternBytes, 0, patternBytes.length,
+                        bytes, i - patternBytes.length, i)) {
+                    return patternTypes.get(pattern);
+                }
+            }
         }
         return "Unknown file type";
+    }
+
+    private String kmp(byte[] bytes) {
+        for (String pattern : patternTypes.keySet()) {
+            var patternBytes = pattern.getBytes();
+            var prefix = getPrefix(patternBytes);
+
+            int j = 0;
+            for (int i = 0; i < bytes.length; i++) {
+                while (j > 0 && bytes[i] != patternBytes[j]) {
+                    j = prefix[j - 1];
+                }
+                if (bytes[i] == patternBytes[j]) {
+                    j += 1;
+                }
+                if (j == patternBytes.length) {
+                    return patternTypes.get(pattern);
+                }
+            }
+        }
+        return "Unknown file type";
+    }
+
+    private int[] getPrefix(byte[] bytes) {
+        var prefix = new int[bytes.length];
+
+        for (int i = 1; i < bytes.length; i++) {
+            int j = prefix[i - 1];
+            while (j > 0 && bytes[i] != bytes[j]) {
+                j = prefix[j - 1];
+            }
+
+            if (bytes[i] == bytes[j]) {
+                j++;
+            }
+
+            prefix[i] = j;
+        }
+        return prefix;
     }
 }
--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -3,9 +3,9 @@ package analyzer;
 public class Main {
     public static void main(String[] args) {
         var analyzer = new Analyzer();
-        analyzer.setFile(args[0]);
-        analyzer.addPattern(args[1], args[2]);
+        analyzer.setFile(args[1]);
+        analyzer.addPattern(args[2], args[3]);
 
-        System.out.println(analyzer.analyze());
+        System.out.println(analyzer.analyze(args[0]));
     }
 }
